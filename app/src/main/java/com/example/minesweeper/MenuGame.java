4aba00ddc51a93182e97f31edeae1f867c0f938a
sapir <sapir.madmon@s.afeka.ac.il>
2020-04-07 12:48:46 +0300

    add activity_menu_game and activity_end_game

---
+++ app/src/main/java/com/example/minesweeper/MenuGame.java
@@ -0,0 +1,14 @@
+package com.example.minesweeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_game);
+    }
+}
