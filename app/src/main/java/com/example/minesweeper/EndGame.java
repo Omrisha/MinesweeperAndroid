5c26a67d7c1f9623d443b9b3d8f4c45441ef1651
sapir <sapir.madmon@s.afeka.ac.il>
2020-04-08 18:15:02 +0300

    exit button and play again button

--- app/src/main/java/com/example/minesweeper/EndGame.java
+++ app/src/main/java/com/example/minesweeper/EndGame.java
@@ -1,7 +1,8 @@
 package com.example.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class EndGame extends AppCompatActivity {
@@ -10,5 +11,25 @@ public class EndGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game);
+
+        //Exit Button
+        findViewById(R.id.buttonExit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+
+        //Play again Button
+        findViewById(R.id.buttonPlayAgain).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EndGame.this,MenuGame.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
