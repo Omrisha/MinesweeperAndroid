2acc557f05d98a70d4bd6c1ffd7d3c6f12a5e789
sapir <sapir.madmon@s.afeka.ac.il>
2020-04-16 19:30:23 +0300

    Adding a test to neighbors that are out of the matrix range

--- app/src/main/java/com/example/minesweeper/logic/Board.java
+++ app/src/main/java/com/example/minesweeper/logic/Board.java
@@ -128,35 +128,35 @@ public class Board {
         //                 SW | S     | SE
         // -----------------------------------------------------------
         // North West
-        if (inRange(index - this.cols - 1)) {
+        if (inRange(index - this.cols - 1) && (index%this.cols != 0) && (index/this.cols != 0)) {
             neighbours.add(this.mTiles[index - this.cols - 1]);
         }
         // North
-        if (inRange(index - this.cols)) {
+        if (inRange(index - this.cols) && (index/this.cols != 0)) {
             neighbours.add(this.mTiles[index - this.cols]);
         }
         // North East
-        if (inRange(index - this.cols + 1)) {
+        if (inRange(index - this.cols + 1) && (index/this.cols != 0) && (index%this.cols != this.cols-1)) {
             neighbours.add(this.mTiles[index - this.cols + 1]);
         }
         // West
-        if (inRange(index - 1)) {
+        if (inRange(index - 1) && (index%this.cols != 0)) {
             neighbours.add(this.mTiles[index - 1]);
         }
         // East
-        if (inRange(index + 1)) {
+        if (inRange(index + 1) && (index%this.cols < this.cols-1)) {
             neighbours.add(this.mTiles[index + 1]);
         }
         // South West
-        if (inRange(index + this.cols - 1)) {
+        if (inRange(index + this.cols - 1)  && (index%this.cols != 0) && (index/this.cols != this.rows-1)) {
             neighbours.add(this.mTiles[index + this.cols - 1]);
         }
         // South
-        if (inRange(index + this.cols)) {
+        if (inRange(index + this.cols) && (index/this.cols < this.rows-1)) {
             neighbours.add(this.mTiles[index + this.cols]);
         }
         // South East
-        if (inRange(index + this.cols + 1)) {
+        if (inRange(index + this.cols + 1) && (index/this.cols != this.rows-1) && (index%this.cols != this.cols-1)) {
             neighbours.add(this.mTiles[index + this.cols + 1]);
         }
         return neighbours;
